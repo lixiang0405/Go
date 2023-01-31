6d4e58a50f3baf9a7265f8fb9882013b48d0fbfa
yiyiのCat <99354406+lixiang0405@users.noreply.github.com>
2023-01-31 23:04:38 +0800

    Add files via upload

---
+++ gee-rpc/xclient/xclient.go
@@ -0,0 +1,174 @@
+package xclient
+
+import (
+	"context"
+	. "geerpc"
+	"io"
+	"log"
+	"net/http"
+	"reflect"
+	"strings"
+	"sync"
+	"time"
+)
+
+type XClient struct {
+	d Discovery
+	mode SelectMode
+	opt *Option
+	mu sync.Mutex
+	clients map[string]*Client
+}
+
+type GeeRegistryDiscovery struct {
+	*MultiServersDiscovery
+	registry string
+	timeout time.Duration
+	lastUpdate time.Time
+}
+
+const defaultUpdateTimeout = time.Second * 10
+
+func NewGeeRegistryDiscovery(registerAddr string, timeout time.Duration) *GeeRegistryDiscovery {
+	if timeout == 0 {
+		timeout = defaultUpdateTimeout
+	}
+	d := &GeeRegistryDiscovery{
+		MultiServersDiscovery: NewMultiServerDiscovery(make([]string, 0)),
+		registry: registerAddr,
+		timeout: timeout,
+	}
+	return d
+}
+
+func (d *GeeRegistryDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.servers = servers
+	d.lastUpdate = time.Now()
+	return nil
+}
+
+func (d *GeeRegistryDiscovery) Refresh() error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	if d.lastUpdate.Add(d.timeout).After(time.Now()) {
+		return nil
+	}
+	log.Println("rpc registry: refresh servers from registry", d.registry)
+	resp, err := http.Get(d.registry)
+	if err != nil {
+		log.Println("rpc registry refresh err:", err)
+		return err
+	}
+	servers := strings.Split(resp.Header.Get("X-Geerpc-Servers"), ",")
+	d.servers = make([]string, 0, len(servers))
+	for _, server := range servers {
+		if strings.TrimSpace(server) != "" {
+			d.servers = append(d.servers, strings.TrimSpace(server))
+		}
+	}
+	d.lastUpdate = time.Now()
+	return nil
+}
+
+func (d *GeeRegistryDiscovery) Get(mode SelectMode) (string, error) {
+	if err := d.Refresh(); err != nil {
+		return "", err
+	}
+	return d.MultiServersDiscovery.Get(mode)
+}
+
+func (d *GeeRegistryDiscovery) GetAll() ([]string, error) {
+	if err := d.Refresh(); err != nil {
+		return nil, err
+	}
+	return d.MultiServersDiscovery.GetAll()
+}
+
+var _ io.Closer = (*XClient)(nil)
+
+func NewXClient(d Discovery, mode SelectMode, opt *Option) *XClient {
+	return &XClient{d: d, mode: mode, opt: opt, clients: make(map[string]*Client)}
+}
+
+func (xc *XClient) Close() error {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+	for key, client := range xc.clients {
+		_ = client.Close()
+		delete(xc.clients, key)
+	}
+	return nil
+}
+
+func (xc *XClient) dial(rpcAddr string) (*Client, error) {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+	client, ok := xc.clients[rpcAddr]
+	if ok && !client.IsAvaildable() {
+		_ = client.Close()
+		delete(xc.clients, rpcAddr)
+		client = nil
+	}
+	if client == nil {
+		var err error
+		client, err = XDial(rpcAddr, xc.opt)
+		if err != nil {
+			return nil, err
+		}
+		xc.clients[rpcAddr] = client
+	}
+	return client, nil
+}
+
+func (xc *XClient) call(rpcAddr string, ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	client, err := xc.dial(rpcAddr)
+	if err != nil {
+		return err
+	}
+	return client.Call(ctx, serviceMethod, args, reply)
+}
+
+func (xc *XClient) Call(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	rpcAddr, err := xc.d.Get(xc.mode)
+	if err != nil {
+		return err
+	}
+	return xc.call(rpcAddr, ctx, serviceMethod, args, reply)
+}
+
+func (xc *XClient) Broadcast(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	servers, err := xc.d.GetAll()
+	if err != nil {
+		return err
+	}
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var e error
+	replyDone := reply == nil
+	ctx, cancle := context.WithCancel(ctx)
+	for _, rpcAddr := range servers {
+		wg.Add(1)
+		go func(rpcAddr string) {
+			defer wg.Done()
+			var clonedReply interface{}
+			if reply != nil {
+				clonedReply = reflect.New(reflect.ValueOf(reply).Elem().Type()).Interface()
+			}
+			err := xc.call(rpcAddr, ctx, serviceMethod, args, clonedReply)
+			mu.Lock()
+			if err != nil && e == nil {
+				e = err
+				cancle()
+			}
+			if err == nil && !replyDone {
+				reflect.ValueOf(reply).Elem().Set(reflect.ValueOf(clonedReply).Elem())
+				replyDone = true
+			}
+			mu.Unlock()
+		}(rpcAddr)
+	}
+	wg.Wait()
+	return e
+}
